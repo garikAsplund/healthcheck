e42976bc02627cbeaebc4e819f1fd45d5647b968
Garik Asplund <hello@garik.codes>
2025-10-26 03:33:28 +0000

    better pm2 settings

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,5 +1,7 @@
 import type { PageLoad } from './$types';
 
+console.log(`App started at ${new Date().toISOString()}`);
+
 export const load: PageLoad = async ({ fetch }) => {
   const res = await fetch('/api/pings');
   const pings = await res.json();
