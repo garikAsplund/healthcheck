ad4b864b944b6f06218a71460abba21f4c7cd54e
Garik Asplund <hello@garik.codes>
2025-11-02 17:00:40 -0800

    change port to avoid conflicts

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
       cwd: "/home/garik/projects/healthcheck",
       env: {
         NODE_ENV: "production",
-	PORT: 3000,
+	PORT: 3001,
         TURSO_DATABASE_URL: "libsql://healthcheck-garikasplund.aws-us-west-2.turso.io",
         TURSO_AUTH_TOKEN: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3NjEzNDg1OTMsImlkIjoiZjZlYmYxOTItYzY0NS00YzNlLWI3MzctZjM2ZDdhZmM0MjVhIiwicmlkIjoiODk4NWFmYzUtYTJhMS00ZjA5LWIzZmMtZTE5NWI0Njk0ZDAyIn0.vqkqh9iyAd-Iu4OVqKX6NO6lcabnG3xtP18CqDl6mvFuscpK7FA3_DqbyW0dkeOQF4hEO8Jacb0cHpzgBpgpBA"
       },
